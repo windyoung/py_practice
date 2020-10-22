4b8ab71a719712bce8d19cf2a63ec407a4dff755
windyoung <windyoung89@gmail.com>
2020-10-22 16:39:47 +0800

    m

--- 1.py
+++ 1.py
@@ -1,12 +1,12 @@
 #-*- coding:utf-8 -*-
-a='\u5b9e\u65f6\u66f4\u65b0\uff1a\u65b0\u578b\u51a0\u72b6\u75c5\u6bd2\u80ba\u708e\u75ab\u60c5\u5730\u56fe'
+
 '''
 @Author: Zhu Jian
 @Date: 2020-04-13 19:16:58
-@LastEditTime: 2020-07-07 15:23:08
-@LastEditors: Zhu Jian
+LastEditTime: 2020-10-22 16:38:36
+LastEditors: Zhu Jian
 @Description: 
-@FilePath: \py_practice\1.py
+FilePath: \py_practice\1.py
 @
 '''
 # print(a)
@@ -18,5 +18,6 @@ a='\u5b9e\u65f6\u66f4\u65b0\uff1a\u65b0\u578b\u51a0\u72b6\u75c5\u6bd2\u80ba\u708
 # print(pandas.__version__)
 # t = ('a', 'c','b','b')
 t = ['a', 'c','b','b']
+a = '\u5b9e\u65f6\u66f4\u65b0\uff1a\u65b0\u578b\u51a0\u72b6\u75c5\u6bd2\u80ba\u708e\u75ab\u60c5\u5730\u56fe'
 t.sort()
-print(t)
+print(t,a)
--- testpinyin.py
+++ testpinyin.py
@@ -2,12 +2,15 @@
 '''
 Author: Zhu Jian
 Date: 2020-10-22 16:32:52
-LastEditTime: 2020-10-22 16:34:49
+LastEditTime: 2020-10-22 16:37:47
 LastEditors: Zhu Jian
 Description: 
 FilePath: \py_practice\testpinyin.py
 
 '''
 import pypinyin as pyp 
-a=pyp.lazy_pinyin("银行很行")
-print(a)
+a = "银行很行"
+t1="经视经视近视镜时"
+b=pyp.lazy_pinyin(a)
+c=pyp.pinyin(t1)
+print(b,c)
